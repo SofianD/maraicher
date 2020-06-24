54cb43887778b66eda9db77f5d190a0c04ee9c79
SofianD <doual.sofian@hotmail.com>
2020-06-24 18:47:46 +0200

    add store crud

--- src/core/store/controller/store.controller.ts
+++ src/core/store/controller/store.controller.ts
@@ -1,4 +1,71 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpException, HttpStatus, Get, Param, Put, Delete } from '@nestjs/common';
+import { StoreService } from '../service/store.service';
 
 @Controller('store')
-export class StoreController {}
+export class StoreController {
+
+    constructor (
+        private readonly storeService: StoreService
+    ) {}
+
+    @Post()
+    async create(
+        @Body('data.store') store
+    ) {
+        let result;
+        try {
+            result = await this.storeService.create(store);
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
+    }
+
+    @Get('/:id')
+    async getOneStore(
+        @Param('id') id
+    ) {
+        let store;
+        try {
+            store = await this.storeService.findById(id);
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return store;
+    }
+
+    @Put('/:id')
+    async updateStore(
+        @Param('id') id: string,
+        @Body('data.store') store
+    ) {
+        let result;
+        try {
+            result = await this.storeService.update(id, store);
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
+    }
+
+    @Delete('/:id')
+    async deleteStore(
+        @Param('id') id: string
+    ) {
+        let result;
+        try {
+            result = await this.storeService.delete(id);
+        } catch (error) {
+            throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (result.deletedCount === 0) {
+            throw new HttpException({ message: 'Deletion failed'}, HttpStatus.BAD_REQUEST);
+        }
+
+        return;
+    }
+}
--- src/core/store/service/store.service.ts
+++ src/core/store/service/store.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { ReturnModelType } from '@typegoose/typegoose';
+import { Store } from 'src/shared/models/core/store.interface';
 
 @Injectable()
-export class StoreService {}
+export class StoreService {
+    constructor(
+        private readonly storeModel: ReturnModelType<typeof Store>
+    ) {}
+
+    async create (data) {
+        const store = new this.storeModel(data);
+
+        let createdStore;
+        try {
+            createdStore = await store.save();
+        } catch (error) {
+            throw error;
+        }
+
+        return createdStore._id;
+    }
+
+    async findById(id: string) {
+        let result;
+        try {
+            result = await this.storeModel.findById(id);
+        } catch (error) {
+            throw error;
+        }
+
+        return result;
+    }
+
+    async update(id, store) {
+        let result;
+
+        try {
+            result = await this.storeModel.findByIdAndUpdate(
+                id,
+                store,
+                {
+                    new: true
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+
+        result = result._doc;
+        return result._id;
+    }
+
+    async delete(id) {
+        let result;
+        try {
+            result = await this.storeModel.findByIdAndDelete(id);
+        } catch (error) {
+            throw error;
+        }
+
+        return result;
+    }
+}
