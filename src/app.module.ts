1fc5e5f657527ce18ca9d338ad22207a63593d74
SofianD <doual.sofian@hotmail.com>
2020-06-24 15:53:32 +0200

    add db link and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    // TypegooseModule.forRoot(
-    //   'ma bdd',
-    //   {
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true
-    //   }
-    // )
+    TypegooseModule.forRoot(
+      "mongodb+srv://server:QLbDkTQGb4pv3uMS@cluster0-tlo9o.mongodb.net/<dbname>?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+      }
+    ),
+    UserModule
   ],
   controllers: [],
   providers: [],
