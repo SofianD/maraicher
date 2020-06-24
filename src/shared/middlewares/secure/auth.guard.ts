c157d3f77b2a5d16d03982e6cb6f43c4648f8fbb
SofianD <doual.sofian@hotmail.com>
2020-06-24 17:12:11 +0200

    create auth guard

---
+++ src/shared/middlewares/secure/auth.guard.ts
@@ -0,0 +1,21 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    
+    const token = context.switchToHttp().getRequest().headers.authorization;
+    return token === undefined ? false :
+    jwt.verify(
+      token,
+      '167CD8DC2E719C1CE671DBAEA8437',
+      (err, decoded) => {
+        return err ? false : true;
+      }
+    ) as any;
+  }
+}
